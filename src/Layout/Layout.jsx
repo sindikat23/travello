d89fd95550d6050cf07d99f07d8fb3a3edda5af1
alpmis <ajoldasov013@gmail.com>
2025-04-14 20:41:54 +0500

    en-uz

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,17 @@
 "use client"
 import { ThemeProvider } from 'next-themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 
 
 
 const Layout = ({ children }) => {
+    const [state, setstate] = useState(false)
+    useEffect(() => {
+        setstate(true)
+
+    });
+    if (!state) return null;
     return (
         <ThemeProvider attribute={'class'} defaultTheme='class'>
             <Navbar/>
